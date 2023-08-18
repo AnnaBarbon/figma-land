5ea33066aa4f90f002c56889c5124434cb3adde3
Anna Barbon <anna.barbon3@gmail.com>
2023-08-18 10:30:12 +0300

    feat: added blocks header, page, links-list, navigation-link, light-link, section-subtitle, button-try-free; and modifiers: links-list_place_header, section-subtitle_place_header, links-list_place_menu

---
+++ js/burger-menu.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('.header__burger-logo').click(function(event){
+    $('.header__menu, .header__burger-logo, .header__burger-line').toggleClass('active');
+  });
+});
